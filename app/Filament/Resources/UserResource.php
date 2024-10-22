6dd419bf16adca1e08a8a2ea4199f352e70b4b6e
Showrabh Rahman <rshowrabh@gmail.com>
2024-10-23 02:12:06 +0600

    adminImage

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -15,8 +15,10 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
 use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class UserResource extends Resource
 {
@@ -28,15 +30,31 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('name'),
-                TextInput::make('email')->email(),
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->email()
+                    ->required(),
                 Forms\Components\Select::make('roles')
                     ->relationship('roles', 'name')
                     ->multiple()
                     ->preload()
+                    ->required()
                     ->searchable(),
-                TextInput::make('password')->password(),
-                TextInput::make('password_confirmation')->same('password')->password()
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+                TextInput::make('password_confirmation')
+                    ->same('password')
+                    ->required('password')
+                    ->password(),
+                FileUpload::make('profile_photo_path')
+                ->avatar()
+                ->label('Profile Photo')
+                ->disk('public')
+                ->directory('profile')
+                ->image()
+                ->visibility('public')
             ]);
     }
 
@@ -44,6 +62,10 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
+                ImageColumn::make('profile_photo_path')
+                ->label('Avatar')
+                ->defaultImageUrl(url('/storage/profile/avatar.jpg'))
+                ->circular(),
                 TextColumn::make('name')->searchable()->sortable(),
                 TextColumn::make('roles.name')
                 ->formatStateUsing(fn (string $state) => Str::of($state)->snake()->replace('_', ' ')->title())
@@ -54,12 +76,23 @@ class UserResource extends Resource
                 SelectFilter::make('roles')->relationship('roles', 'name')
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\DeleteAction::make()
+                ->after(function (User $record) {
+                    if ($record->profile_photo_path) {
+                       Storage::disk('public')->delete($record->profile_photo_path);
+                    }
+                 }),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make()
+                    ->after(function (User $record) {
+                        if ($record->profile_photo_path) {
+                           foreach ($record->profile_photo_path as $ph) Storage::disk('public')->delete($ph);
+                        }
+                     }),
                 ]),
             ])
             ->striped();
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,8 +10,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\HasAvatar;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable implements FilamentUser, HasAvatar
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles, HasPanelShield;
@@ -25,15 +26,12 @@ class User extends Authenticatable implements FilamentUser
         'name',
         'email',
         'password',
+        'profile_photo_path'
     ];
     // public function canAccessPanel(Panel $panel): bool
     // {
     //     return str_ends_with($this->email, '@a.com') && $this->hasVerifiedEmail();
     // }
-    public function getFilamentAvatarUrl(): ?string
-    {
-        return $this->avatar_url;
-    }
 
     /**
      * The attributes that should be hidden for serialization.
@@ -56,5 +54,35 @@ class User extends Authenticatable implements FilamentUser
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($record) {
+            if ($record->isDirty('profile_photo_path')) {
+                $oldAvatar = $record->getOriginal('profile_photo_path');
+                if ($oldAvatar) {
+                    Storage::disk('public')->delete($oldAvatar);
+                }
+            }
+        });
+    }
+
+    /**
+     * Get the user's avatar URL.
+     */
+    public function getFilamentAvatarUrl(): ?string
+    {
+        return $this->profile_photo_url;
+    }
+
+    /**
+     * Used in filamnet resource to display user's roles.
+     */
+    public function getRoleNamesAttribute(): string
+    {
+        return $this->roles->pluck('name')->join(',');
     }
 }
